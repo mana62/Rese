55597bbba990c93312bf88c10f587281726bae3d
Mana Fukumoto <mana66354@gmail.com>
2024-10-24 23:01:23 +0900

    環境構築

---
+++ src/app/Models/Restaurant.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'area_id',
+        'genre_id',
+        'description',
+    ];
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+}
